8de28420175bbcca5a66d3813145109f551e5a25
Phillip Lopez <pgl5711@rit.edu>
2017-03-19 19:30:26 -0400

    Initial Commit

---
+++ DotaMining/src/main/java/cs721/DotaMining/App.java
@@ -0,0 +1,13 @@
+package cs721.DotaMining;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
