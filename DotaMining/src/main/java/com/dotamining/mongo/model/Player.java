521613144c7bec7c48d15e3b1d39825eeceb7a21
Phillip Lopez <pgl5711@rit.edu>
2017-03-19 21:20:09 -0400

    Created basic player object

--- DotaMining/src/main/java/com/dotamining/mongo/model/Player.java
+++ DotaMining/src/main/java/com/dotamining/mongo/model/Player.java
@@ -7,5 +7,71 @@ public class Player extends DotaObject{
 	public Player(Document doc) {
 		super(doc);
 	}
-
+	
+	public Integer getPlayerSlot(){
+		return getDoc().getInteger("player_slot");
+	}
+	
+	public Integer getMatchID(){
+		return getDoc().getInteger("match_id");
+	}
+	
+	public Integer getAccountID(){
+		return getDoc().getInteger("account_id");
+	}
+	
+	public Integer getAssists(){
+		return getDoc().getInteger("assists");
+	}
+	
+	public Integer getKills(){
+		return getDoc().getInteger("kills");
+	}
+	
+	public Integer getLastHits(){
+		return getDoc().getInteger("last_hits");
+	}
+	
+	public Integer getDeaths(){
+		return getDoc().getInteger("assists");
+	}
+	
+	public Integer getGoldPerMin(){
+		return getDoc().getInteger("gold_per_min");
+	}
+	
+	public Integer getGoldSpent(){
+		return getDoc().getInteger("gold_spent");
+	}
+	
+	public Integer getHeroDamage(){
+		return getDoc().getInteger("hero_damage");
+	}
+	
+	public Integer getHeroID(){
+		return getDoc().getInteger("hero_id");
+	}
+	
+	/**
+	 * 0   - Played Full Game
+	 * > 0 - Abandoned
+	 * @return
+	 */
+	public Integer getLeaverStatus(){
+		return getDoc().getInteger("leave_status");
+	}
+	
+	public Integer getLevel(){
+		return getDoc().getInteger("level");
+	}
+	
+	public Integer getStuns(){
+		return getDoc().getInteger("stuns");
+	}
+	
+	public Integer xpPerMin(){
+		return getDoc().getInteger("xp_per_min");
+	}
+	
+	
 }
