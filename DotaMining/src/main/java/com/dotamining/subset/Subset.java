5b235b794c1f4371659f8a7fdb0b2ddd2923edad
mmbohari <iib8085@rit.edu>
2017-04-24 16:37:36 -0400

    Added functions to make a subset of Players and Chat dataset

--- DotaMining/src/main/java/com/dotamining/subset/Subset.java
+++ DotaMining/src/main/java/com/dotamining/subset/Subset.java
@@ -82,6 +82,127 @@ public class Subset {
 		}
 	}
 	
+	public void createSubsetPlayer(String path){
+		
+		try {
+			br = new BufferedReader(new FileReader(path));
+			
+			fw = new FileWriter("/home/mmbohari/Desktop/Link to Spring 2017"
+					+ "/Data Cleaning & Preparation/Term Project/Phase 3"
+					+ "/Subset/Players.csv");
+			bw = new BufferedWriter(fw);
+			
+			line = br.readLine();
+			bw.write(line);
+			bw.newLine();
+			
+			while(true){
+				line = br.readLine();
+				String [] array = line.split(splitBy);
+				if(array[1].equals("5000"))
+					break;
+				bw.write(line);
+				bw.newLine();
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("File not Found; Given path: " + path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			
+			// close buffer reader
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            // close buffer writer and file writer
+			try {
+
+				if (bw != null)
+					bw.close();
+
+				if (fw != null)
+					fw.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}
+	}
+	
+	public void createSubsetChat(String path){
+		
+		try {
+			br = new BufferedReader(new FileReader(path));
+			
+			fw = new FileWriter("/home/mmbohari/Desktop/Link to Spring 2017"
+					+ "/Data Cleaning & Preparation/Term Project/Phase 3"
+					+ "/Subset/Chat.csv");
+			bw = new BufferedWriter(fw);
+			
+			line = br.readLine();
+			bw.write(line);
+			bw.newLine();
+			
+			while(true){
+				line = br.readLine();
+				String [] array = line.split(splitBy);
+				if(array[1].equals("5000"))
+					break;
+				if( ( (array[array.length-1].equals("true")) || (array[array.length-1].equals("false")) ) ){
+					bw.write(line);
+					bw.newLine();
+				}
+				
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("File not Found; Given path: " + path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			
+			// close buffer reader
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            // close buffer writer and file writer
+			try {
+
+				if (bw != null)
+					bw.close();
+
+				if (fw != null)
+					fw.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}
+	}
+	
 	public static void main(String[] args) {
 		Subset obj = new Subset();
 		
@@ -90,8 +211,15 @@ public class Subset {
 				+ "/Data Cleaning & Preparation/Term Project/Phase 3"
 				+ "/Processed Datase v1/Match.csv");
 		
-		// Create a subset of Match.csv
-
+		// Create a subset of Player.csv
+		obj.createSubsetPlayer("/home/mmbohari/Desktop/Link to Spring 2017"
+				+ "/Data Cleaning & Preparation/Term Project/Phase 3"
+				+ "/Processed Datase v1/Players.csv");
+		
+		// Create a subset of Chat.csv
+		obj.createSubsetChat("/home/mmbohari/Desktop/Link to Spring 2017"
+				+ "/Data Cleaning & Preparation/Term Project/Phase 3"
+				+ "/Processed Datase v1/Chat.csv");
 	}
 
 }
