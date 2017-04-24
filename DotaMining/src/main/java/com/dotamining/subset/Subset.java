0dd9be6e918a83a3c6ffb60d4864ed11aff0997b
mmbohari <iib8085@rit.edu>
2017-04-24 15:35:46 -0400

    Added a class which creates subset of Match.csv

---
+++ DotaMining/src/main/java/com/dotamining/subset/Subset.java
@@ -0,0 +1,97 @@
+package com.dotamining.subset;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/*
+ * 
+ * This class will create a subset dataset from the original dataset.
+ * The subset data set will contain instances from match_id 0 to 4999
+ * i.e. 5000 matches are being considered
+ * 
+ */
+
+public class Subset {
+
+    BufferedReader br = null;
+	BufferedWriter bw = null;
+	FileWriter fw = null;
+	String line;
+	String splitBy = ",";
+	
+	public void createSubsetMatch(String path){
+		
+		try {
+			br = new BufferedReader(new FileReader(path));
+			
+			fw = new FileWriter("/home/mmbohari/Desktop/Link to Spring 2017"
+					+ "/Data Cleaning & Preparation/Term Project/Phase 3"
+					+ "/Subset/Match.csv");
+			bw = new BufferedWriter(fw);
+			
+			line = br.readLine();
+			bw.write(line);
+			bw.newLine();
+			
+			while(true){
+				line = br.readLine();
+				String [] array = line.split(splitBy);
+				if(array[0].equals("5000"))
+					break;
+				bw.write(line);
+				bw.newLine();
+			}
+			
+			
+			
+		} catch (FileNotFoundException e) {
+			System.err.println("File not Found; Given path: " + path);
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			
+			// close buffer reader
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            // close buffer writer and file writer
+			try {
+
+				if (bw != null)
+					bw.close();
+
+				if (fw != null)
+					fw.close();
+
+			} catch (IOException ex) {
+
+				ex.printStackTrace();
+
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Subset obj = new Subset();
+		
+		// Create a subset of Match.csv
+		obj.createSubsetMatch("/home/mmbohari/Desktop/Link to Spring 2017"
+				+ "/Data Cleaning & Preparation/Term Project/Phase 3"
+				+ "/Processed Datase v1/Match.csv");
+		
+		// Create a subset of Match.csv
+
+	}
+
+}
